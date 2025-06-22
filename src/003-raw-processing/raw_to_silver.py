116bb310269b6f94b7c577803e04119a4d0ed79f
msap89 <152655536+msap89@users.noreply.github.com>
2025-06-22 10:42:07 -0300

    Add files via upload
    commit para incluir códigos de extração e tratamento desenvolvidos no Databricks.

---
+++ src/003-raw-processing/raw_to_silver.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+# MAGIC %run ../../src/001-common/swiss_knife_taxi
+
+# COMMAND ----------
+
+dbutils.widgets.text("types", "yellow,green,fhv", "Types")
+dbutils.widgets.text("months", "01,02,03,04,05", "Months")
+dbutils.widgets.text("years", "2023", "Years")
+dbutils.widgets.text("raw_base", "dbfs:/FileStore/raw/TLC_NYC/", "Raw_Base")
+dbutils.widgets.text("silver_base", "dbfs:/FileStore/silver/TLC_NYC/", "Silver_Base")
+
+# COMMAND ----------
+
+types = dbutils.widgets.get("types").split(",")
+months = dbutils.widgets.get("months").split(",")
+years = dbutils.widgets.get("years").split(",")
+raw_base = dbutils.widgets.get("raw_base")
+silver_base = dbutils.widgets.get("silver_base")
+
+raw_to_silver(types, years, months, raw_base, silver_base)
