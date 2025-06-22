102c52dc94a73339e8923697f8dfb6c2807ba3a0
msap89 <152655536+msap89@users.noreply.github.com>
2025-06-22 10:41:11 -0300

    Add files via upload
    commit para incluir códigos de extração e tratamento desenvolvidos no Databricks.

---
+++ src/002-ingestion/external_data_to_raw.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %run ../../src/001-common/swiss_knife_taxi
+
+# COMMAND ----------
+
+dbutils.widgets.text("types", "yellow,green,fhv", "Types")
+dbutils.widgets.text("months", "01,02,03,04,05", "Months")
+dbutils.widgets.text("years", "2023", "Years")
+
+# COMMAND ----------
+
+types = dbutils.widgets.get("types").split(",")
+months = dbutils.widgets.get("months").split(",")
+years = dbutils.widgets.get("years").split(",")
+
+#chamada da função
+ingest_taxi_data(types, years, months)
